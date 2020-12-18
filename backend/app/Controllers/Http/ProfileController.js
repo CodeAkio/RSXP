ac8ac648db238b59a6f80ab3af494367e77fa048
CodeAkio <siqueira_2004@hotmail.com>
2020-12-18 16:02:36 -0300

    Update profile

--- backend/app/Controllers/Http/ProfileController.js
+++ backend/app/Controllers/Http/ProfileController.js
@@ -2,9 +2,11 @@ const Helpers = use('Helpers');
 
 class ProfileController {
   async update({ request, auth }) {
-    const user = await auth.getUser();
-
+    const data = request.only(['name', 'title', 'bio', 'github', 'linkedin']);
+    const password = request.input('password');
     const avatar = request.file('avatar');
+
+    const user = await auth.getUser();
 
     if (avatar) {
       await avatar.move(Helpers.tmpPath('files'), {
@@ -15,6 +17,12 @@ class ProfileController {
       if (!avatar.moved()) return avatar.error();
 
       user.avatar = avatar.fileName;
+    }
+
+    user.merge(data);
+
+    if (password) {
+      user.password = password;
     }
 
     await user.save();
--- backend/app/Validators/Profile.js
+++ backend/app/Validators/Profile.js
@@ -7,6 +7,8 @@ class Profile {
 
   get rules() {
     return {
+      name: 'required',
+      password: 'confirmed',
       avatar: 'file|file_ext:png,jpg,jpeg|file_size:2mb|file_types:image',
     };
   }
